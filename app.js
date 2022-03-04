db44be0671d154e0a746e59692862a2fb97a40df
Muneeba Dilawaze <muneeba.dilawaze@gmail.com>
2022-03-04 14:01:43 +0500

    added CRUD functionality + JWT

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 require('express-async-errors');
 const express = require('express');
 const app = express();
-
+const authenticateUser = require("./middleware/authentication")
 //connectDB
 
 const connectDB = require("./db/connect")
@@ -23,7 +23,7 @@ app.use(express.json());
 // routes
 
 app.use("/api/v1/auth",authRouter)
-app.use("/api/v1/jobs",jobsRouter)
+app.use("/api/v1/jobs",authenticateUser,jobsRouter)
 
 
 app.use(notFoundMiddleware);
